435ee35abe36f4b732f02843480dfbff7bebb929
flywithbug <369495368@qq.com>
2018-12-21 15:53:18 +0800

    store

---
+++ web_client/src/store/getters.js
@@ -0,0 +1,7 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  language: state => state.app.language,
+  size: state => state.app.size,
+  device: state => state.app.device,
+}
+export default getters
