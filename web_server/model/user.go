5cf4e82fe62b799e96e169d6cd7c885fd9ac9111
flywithbug <369495368@qq.com>
2018-12-27 17:56:43 +0800

    register

--- web_server/model/user.go
+++ web_server/model/user.go
@@ -16,26 +16,29 @@ const (
 )
 
 type User struct {
-	Id       int64     `json:"_id" bson:"_id"`
-	UserId   string    `json:"user_id" bson:"user_id"`
-	Account  string    `json:"account"`
-	Password string    `json:"password,omitempty"`
-	Avatar   string    `json:"avatar,omitempty"`
-	Email    string    `json:"email,omitempty"`
-	Phone    string    `json:"phone,omitempty"`
-	Sex      int       `json:"sex"` // 0保密，1男 2女
-	RealName string    `json:"real_name" bson:"real_name"`
-	Name     string    `json:"name,omitempty" bson:"name"` //昵称
-	Title    string    `json:"title,omitempty"`
-	Role     roleState `json:"role"`            //用于前端路由配置 1 管理员， 2 普通用户，
-	Roles    []string  `json:"roles,omitempty"` //角色数组
-	Status   int       `json:"status"`
+	Id       int64     `json:"_id" bson:"_id,omitempty"`
+	UserId   string    `json:"user_id" bson:"user_id,omitempty"`
+	Account  string    `json:"account" bson:"account,omitempty"`
+	Password string    `json:"password,omitempty" bson:"password,omitempty"`
+	Avatar   string    `json:"avatar,omitempty bson:"avatar,omitempty"`
+	Email    string    `json:"email,omitempty" bson:"email,omitempty"`
+	Phone    string    `json:"phone,omitempty" bson:"phone,omitempty"`
+	Sex      int       `json:"sex,omitempty" bson:"sex,omitempty"` // 0保密，1男 2女
+	RealName string    `json:"real_name,omitempty" bson:"real_name,omitempty"`
+	Name     string    `json:"name,omitempty" bson:"name,omitempty"` //昵称
+	Title    string    `json:"title,omitempty" bson:"title,omitempty"`
+	Role     roleState `json:"role,omitempty" bson:"role,omitempty"`   //用于前端路由配置 1 管理员， 2 普通用户，
+	Roles    []string  `json:"roles,omitempty" bson:"roles,omitempty"` //角色数组
+	Status   int       `json:"status,omitempty" bson:"status,omitempty"`
 }
 
 var (
 	userC = User{}
 )
 
+/*
+	OperationModel
+*/
 func (u User) Insert(docs ...interface{}) error {
 	return mongo.Insert(db, userCollection, docs...)
 }
@@ -68,34 +71,44 @@ func (u User) RemoveAll(selector interface{}) error {
 	return mongo.RemoveAll(db, userCollection, selector)
 }
 
+/*
+	userModify
+*/
 func FindAllUsers() (*[]User, error) {
 	return userC.FindAll(nil, bson.M{"password": 0})
 }
 
-func (u User) UserInsert() error {
-	if mongo.IsExist(db, userCollection, bson.M{"account": u.Account}) {
+func (u *User) UserInsert() error {
+	if u.IsExist(bson.M{"account": u.Account}) {
 		return errors.New("account 已存在")
 	}
-	if mongo.IsExist(db, userCollection, bson.M{"mail": u.Email}) {
-		return errors.New("mail 已存在")
+	if u.IsExist(bson.M{"email": u.Email}) {
+		return errors.New("email 已存在")
 	}
 	if u.UserId == "" {
 		u.Id, _ = mongo.GetIncrementId("user")
 		u.UserId = bson.NewObjectId().Hex()
 	}
+	if u.Role == 0 {
+		u.Role = 2
+		u.Roles = makeUserRoles(u.Role)
+	}
 	return userC.Insert(u)
 }
 
 func LoginUser(account, pass string) (user *User, err error) {
-	return userC.FindOne(bson.M{"account": account, "password": pass}, bson.M{"password": 0})
-
+	user, err = userC.FindOne(bson.M{"account": account, "password": pass}, bson.M{"password": 0})
+	user.Roles = makeUserRoles(user.Role)
+	return
 }
 
 func FindByUserId(userId string) (u *User, err error) {
-	return userC.FindOne(bson.M{"user_id": userId}, bson.M{"password": 0})
+	u, err = userC.FindOne(bson.M{"user_id": userId}, bson.M{"password": 0})
+	u.Roles = makeUserRoles(u.Role)
+	return
 }
 
-func MakeUserRoles(role roleState) []string {
+func makeUserRoles(role roleState) []string {
 	if role == roleStateTypeNormal {
 		return []string{"normal"}
 	} else if role == roleStateTypeAdmin {
