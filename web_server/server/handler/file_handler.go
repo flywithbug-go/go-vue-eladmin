d3af164450b78a435997ca7d605740e30164f694
flywithbug <369495368@qq.com>
2018-12-27 12:07:07 +0800

    upload api 优化，使用文件md5值作为文件名保存

--- web_server/server/handler/file_handler.go
+++ web_server/server/handler/file_handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"image"
@@ -8,6 +10,7 @@ import (
 	"image/jpeg"
 	"image/png"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -40,48 +43,72 @@ func uploadImageHandler(c *gin.Context) {
 		aRes.SetErrorInfo(http.StatusBadRequest, fmt.Sprintf("get file err : %s", err.Error()))
 		return
 	}
-
-	//if header.Size > 1024*170 {
-	//	aRes.SetErrorInfo(http.StatusRequestEntityTooLarge,fmt.Sprintf(" file to big no more than 150kb "))
-	//	return
-	//}
-	month := time.Now().Format("2006-01")
-	localPath := localImageFilePath + month + "/"
+	defer file.Close()
 	//获取文件名
 	ext := filepath.Ext(header.Filename)
-	name := uuid.New()
-	filename := name + ext
+
+	//获取文件的md5值
+	data, err := ioutil.ReadAll(file)
+	h := md5.New()
+	h.Write(data)
+	value := h.Sum(nil)
+	fileName := hex.EncodeToString(value) + ext
+
 	//文件夹创建管理
-	bExit, err := PathExists(localPath)
+	month := time.Now().Format("2006-01")
+	localPath := localImageFilePath + month + "/"
+	//文件路径
+	localFilePath := localPath + fileName
+	bExit, err := PathExists(localFilePath)
+
 	if err != nil {
 		log4go.Info(err.Error())
-		aRes.SetErrorInfo(http.StatusBadRequest, fmt.Sprintf("get folder err : %s", err.Error()))
+		aRes.SetErrorInfo(http.StatusInternalServerError, fmt.Sprintf("system err : %s", err.Error()))
 		return
 	}
-	if !bExit {
-		err = os.Mkdir(localPath, os.ModePerm)
+	if bExit {
+		log4go.Info("fileExit: %s", fileName)
+		avatarPath := fmt.Sprintf("filename=%s&dir=%s", fileName, month)
+		aRes.SetResponseDataInfo("imagePath", avatarPath)
+		return
+	}
+	out, err := os.Create(localFilePath)
+	if err != nil {
+		log4go.Info("创建文件失败：%s", err.Error())
+		//判断文件夹是否存在
+		bExit, err = PathExists(localPath)
 		if err != nil {
 			log4go.Info(err.Error())
-			aRes.SetErrorInfo(http.StatusBadRequest, fmt.Sprintf("make folder err : %s", err.Error()))
+			aRes.SetErrorInfo(http.StatusInternalServerError, fmt.Sprintf("get folder err : %s", err.Error()))
+			return
+		}
+		//文件夹不存在创建文件夹
+		if !bExit {
+			err = os.Mkdir(localPath, os.ModePerm)
+			if err != nil {
+				log4go.Info(err.Error())
+				aRes.SetErrorInfo(http.StatusInternalServerError, fmt.Sprintf("make folder err : %s", err.Error()))
+				return
+			}
+		}
+		//重新启动out
+		out, err = os.Create(localFilePath)
+		if err != nil {
+			log4go.Info(err.Error())
+			aRes.SetErrorInfo(http.StatusInternalServerError, fmt.Sprintf("make file err : %s", err.Error()))
 			return
 		}
 	}
-	out, err := os.Create(localPath + filename)
-	if err != nil {
-		log4go.Info(err.Error())
-		aRes.SetErrorInfo(http.StatusBadRequest, fmt.Sprintf("create file err : %s", err.Error()))
-		return
-	}
 	defer out.Close()
-	_, err = io.Copy(out, file)
+
+	_, err = out.Write(data)
 	if err != nil {
 		log4go.Info(err.Error())
 		aRes.SetErrorInfo(http.StatusBadRequest, fmt.Sprintf("write file err : %s", err.Error()))
 		return
 	}
-	avatarPath := fmt.Sprintf("filename=%s&dir=%s", filename, month)
+	avatarPath := fmt.Sprintf("filename=%s&dir=%s", fileName, month)
 	aRes.SetResponseDataInfo("imagePath", avatarPath)
-
 }
 
 func makeFilePath(ext string) (string, error) {
