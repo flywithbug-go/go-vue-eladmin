4db17b6c19678394c2ce99c4bbf38c1163dec1fc
flywithbug <369495368@qq.com>
2019-01-08 18:28:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
-
-
-cd web_client
-npm run build:prod
-
-scp -r dist root@118.89.108.25:/root/vue-admin/
-cd ..
-
-cd web_server
-sh autobuild.sh
+case $1 in
+        server)
+            cd web_server
+            sh autobuild.sh
+        ;;
+        vue)
+            cd web_client
+            npm run build:prod
+            scp -r dist root@118.89.108.25:/root/vue-admin/
+        ;;
+        all)
+            cd web_client
+            npm run build:prod
+            scp -r dist root@118.89.108.25:/root/vue-admin/
+            cd ..
+            cd web_server
+            sh autobuild.sh
+        ;;
+        *)
+                echo "$0 {server|vue|all}"
+                exit 4
+        ;;
+esac
