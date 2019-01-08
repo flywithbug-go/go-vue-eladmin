4b02543c13186888be25a105628f9e596b52b5cb
flywithbug <369495368@qq.com>
2019-01-09 07:59:59 +0800

    doman

--- web_client/config/prod.env.js
+++ web_client/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://118.89.108.25:9801/api"'
+  BASE_API: '"http://www.flywithme.top/api"'
 }
--- web_client/config/sit.env.js
+++ web_client/config/sit.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sit"',
-  BASE_API: '"http://118.89.108.25:9801/api"'
+  BASE_API: '"http://www.flywithme.top/api"'
 }
