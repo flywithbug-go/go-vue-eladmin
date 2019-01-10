dc6a1d3690f6403148939350d8515401113a1bf0
flywithbug <369495368@qq.com>
2019-01-11 05:03:02 +0800

    sync map

--- web_server/server/sync/map.go
+++ web_server/server/sync/map.go
@@ -1,28 +1,20 @@
 package sync
 
-import "sync"
+import "vue-admin/web_server/concurrent-map"
 
-//TODO 多线程操作加锁之后变成了单线程，  需要优化为真正的多线程操作，
-var syncMap = struct {
-	sync.RWMutex
-	m map[string]bool
-}{m: make(map[string]bool)}
+var (
+	syncMap = cmap.New()
+)
 
 func SetKeyValue(key string) {
-	syncMap.RLock()
-	syncMap.m[key] = true
-	syncMap.RUnlock()
+	syncMap.Set(key, true)
 }
 
 func RemoveKey(key string) {
-	syncMap.RLock()
-	delete(syncMap.m, key)
-	syncMap.RUnlock()
+	syncMap.Remove(key)
 }
 
 func Value(key string) bool {
-	syncMap.RLock()
-	n := syncMap.m[key]
-	syncMap.RUnlock()
-	return n
+	_, b := syncMap.Get(key)
+	return b
 }
