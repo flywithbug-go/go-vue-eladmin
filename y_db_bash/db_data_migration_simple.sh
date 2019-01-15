b5b1bc5b7b0db712261abe61a74b945cadef9d13
flywithbug <369495368@qq.com>
2019-01-15 11:00:28 +0800

    shell 部署

--- y_db_bash/db_data_migration_simple.sh
+++ y_db_bash/db_data_migration_simple.sh
@@ -5,7 +5,7 @@
 mongodump -d  doc_manager -o ./db_backup/
 
 #拷贝数据库到远端文
-scp  -r ./db_backup/doc_manager root@118.89.108.25:/root/dump
+scp  -r ./db_backup/doc_manager name@host:/root/dump
 
 #远程恢复数据库内容   数据库地址和端口           本地数据库目录            数据库用户名 密码     数据库名    覆盖远端数据库
 mongorestore -h host:port ./db_backup/doc_manager -u name -p pass -d docmanager  --drop
