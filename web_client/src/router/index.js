d5b43507f067c6f95777fbc500ad8515c961b482
flywithbug <369495368@qq.com>
2019-01-16 20:18:48 +0800

    static test

--- web_client/src/router/index.js
+++ web_client/src/router/index.js
@@ -72,47 +72,6 @@ export default new Router({
   routes: constantRouterMap,
   mode: 'history'
 })
-//
-// export const asyncRouterMap = [
-//   userRouter,
-//   metadataRouter,
-//   {
-//     path: '/icon',
-//     component: Layout,
-//     children: [
-//       {
-//         path: 'index',
-//         component: () => import('@/views/svg-icons/index'),
-//         name: 'Icons',
-//         meta: { title: 'icons', icon: 'icon', noCache: true }
-//       }
-//     ]
-//   },
-//   {
-//     path: '/error',
-//     component: Layout,
-//     redirect: 'noredirect',
-//     name: 'ErrorPages',
-//     meta: {
-//       title: 'errorPages',
-//       icon: '404'
-//     },
-//     children: [
-//       {
-//         path: '401',
-//         component: () => import('@/views/errorPage/401'),
-//         name: 'Page401',
-//         meta: { title: 'page401', noCache: true }
-//       },
-//       {
-//         path: '404',
-//         component: () => import('@/views/errorPage/404'),
-//         name: 'Page404',
-//         meta: { title: 'page404', noCache: true, }
-//       }
-//     ]
-//   },
-// ]
 
 
 
