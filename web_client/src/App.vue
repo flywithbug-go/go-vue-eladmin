04fa7362111298238101ac82a983c589791405d3
flywithbug <369495368@qq.com>
2019-01-17 10:52:29 +0800

    -

--- web_client/src/App.vue
+++ web_client/src/App.vue
@@ -11,11 +11,4 @@ export default {
 </script>
 
 <style>
-/*#app {*/
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  /*color: #2d3a4b;*/
-  /*margin:10px;*/
-/*}*/
 </style>
