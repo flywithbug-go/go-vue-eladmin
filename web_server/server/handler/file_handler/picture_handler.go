55a3f72a8047b270b619c7eccff9cf30994add55
flywithbug <369495368@qq.com>
2019-01-24 14:26:09 +0800

    0_0，图片处理

--- web_server/server/handler/file_handler/picture_handler.go
+++ web_server/server/handler/file_handler/picture_handler.go
@@ -1,25 +1,17 @@
 package file_handler
 
 import (
-	"errors"
 	"fmt"
-	"image/gif"
-	"image/jpeg"
-	"image/png"
-	"io"
 	"net/http"
 	"vue-admin/web_server/common"
 	"vue-admin/web_server/server/handler/handler_common"
 
-	"github.com/flywithbug/log4go"
-	"github.com/nfnt/resize"
-	"golang.org/x/image/bmp"
-
 	"vue-admin/web_server/model"
+
+	"github.com/flywithbug/log4go"
 
 	"github.com/gin-gonic/gin"
 
-	"image"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
@@ -64,77 +56,5 @@ func loadImageHandler(c *gin.Context) {
 		return
 	}
 	http.ServeFile(c.Writer, c.Request, imgPath)
-	//
-	//fileOrigin := localImageDirPath + path + "/" + filename
-	//sizeW, err := strconv.Atoi(size)
-	//
-	//if len(size) == 0 || err != nil {
-	//	http.ServeFile(c.Writer, c.Request, fileOrigin)
-	//	return
-	//}
-	//ext := filepath.Ext(filename)
-	//if strings.EqualFold(ext, ".gif") {
-	//	http.ServeFile(c.Writer, c.Request, fileOrigin)
-	//	return
-	//}
-	//
-	//filePath := localImageDirPath + path + "/" + size + "-" + filename
-	//if !file.FileExists(filePath) {
-	//	if !file.FileExists(fileOrigin) {
-	//		c.Writer.Write([]byte("Error: Image Not found."))
-	//		return
-	//	}
-	//
-	//	fIn, _ := os.Open(fileOrigin)
-	//	//log4go.Info(handler_common.RequestId(c) + fileOrigin)
-	//	defer fIn.Close()
-	//	fOut, _ := os.Create(filePath)
-	//	//log4go.Info(handler_common.RequestId(c) + filename)
-	//	defer fOut.Close()
-	//	if sizeW < 100 {
-	//		sizeW = 100
-	//	}
-	//	err := scale(fIn, fOut, sizeW, 100)
-	//	if err != nil {
-	//		log4go.Info(handler_common.RequestId(c) + err.Error())
-	//		http.ServeFile(c.Writer, c.Request, fileOrigin)
-	//		return
-	//	}
-	//}
-	//
-	//http.ServeFile(c.Writer, c.Request, filePath)
-}
 
-func scale(in io.Reader, out io.Writer, size, quality int) error {
-	origin, fm, err := image.Decode(in)
-	if err != nil {
-		return err
-	}
-	width := size
-	height := 0
-	if size == 0 {
-		width = origin.Bounds().Max.X
-		height = origin.Bounds().Max.Y
-	} else {
-		height = origin.Bounds().Max.Y * (size / origin.Bounds().Max.X)
-	}
-
-	if quality == 0 {
-		quality = 100
-	}
-	canvas := resize.Thumbnail(uint(width), uint(height), origin, resize.Lanczos3)
-
-	switch fm {
-	case "jpeg":
-		return jpeg.Encode(out, canvas, &jpeg.Options{Quality: quality})
-	case "png":
-		return png.Encode(out, canvas)
-	case "gif":
-		return gif.Encode(out, canvas, &gif.Options{})
-	case "bmp":
-		return bmp.Encode(out, canvas)
-	default:
-		return errors.New("ERROR FORMAT")
-	}
-	return nil
 }
