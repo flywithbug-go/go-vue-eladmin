57dce7909245583b7c220356a41bd64cac52026b
flywithbug <369495368@qq.com>
2019-02-13 18:39:16 +0800

    no message

--- y_db_bash/db_data_restore.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#//远程拉去数据
-#mongodump -h 118.89.108.25:27017 -u doc -p doc11121014a -d docmanager  -o ./db_backup
-
-//同步到本地数据库
-mongorestore ./db_backup/docmanager -d doc_manager --drop
-
